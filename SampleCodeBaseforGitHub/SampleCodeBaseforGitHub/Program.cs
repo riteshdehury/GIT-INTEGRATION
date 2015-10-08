24f17eebfe038c84931a08d9372ff7d1dcd0fa01
ayandebnath67 <ayan.debnath1@construction.com>
2015-10-08 06:27:08 -0400

    Initial Version

---
+++ SampleCodeBaseforGitHub/SampleCodeBaseforGitHub/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SampleCodeBaseforGitHub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string oputput = GetValue("Hello World");
+        }
+
+        public static string GetValue(string input)
+        {
+            return input;
+        }
+    }
+
+
+}
